db932aea4d36c2aa72cc0a2c0ead100e9dbbb07b
Ali Onur Puskullu <puskullualionur@gmail.com>
2022-06-20 14:59:23 +0300

    Readme Content

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String inputString = "Welcome to 30 Days of Code";
+
+            Console.WriteLine("Hello, World.");
+            Console.WriteLine(inputString);
+            inputString = Console.ReadLine();
+           
+        }
+    }
+}
